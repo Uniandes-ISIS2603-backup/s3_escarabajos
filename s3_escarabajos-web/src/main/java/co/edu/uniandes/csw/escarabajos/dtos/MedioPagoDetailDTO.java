f89c3dc9086272c3e11f6ba0bd79a8cd2bfe1b7a
Mateo Devia <mateodevia2@hotmail.com>
2018-02-11 02:27:53 -0500

    agregue comentarios a las clases

--- s3_escarabajos-web/src/main/java/co/edu/uniandes/csw/escarabajos/dtos/MedioPagoDetailDTO.java
+++ s3_escarabajos-web/src/main/java/co/edu/uniandes/csw/escarabajos/dtos/MedioPagoDetailDTO.java
@@ -6,7 +6,9 @@
 package co.edu.uniandes.csw.escarabajos.dtos;
 
 /**
- *
+ * Clase que extiende de {@link MedioPagoDTO} para manejar la transformacion entre
+ * los objetos JSON y las Entidades de la base de datos. Para conocer el
+ * contenido de la ciudad vaya a la documentacion de {@link MedioPagoDTO}
  * @author Mateo
  */
 public class MedioPagoDetailDTO {
