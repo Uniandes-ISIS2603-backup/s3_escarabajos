7a6c9b8849a81526cc9ba500b012a57650f601f0
n.gaitan <n.gaitan@ISIS2603S3-0061.sis.virtual.uniandes.edu.co>
2018-02-14 10:54:39 -0500

    Calificacion y Modelo Check

---
+++ s3_escarabajos-back/src/main/java/co/edu/uniandes/csw/escarabajos/entities/ReclamoEntity.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.escarabajos.entities;
+
+/**
+ *
+ * @author n.gaitan
+ */
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class ReclamoEntity implements Serializable
+{
+    @Id
+    private Integer id;
+    private List<String> pics;
+    private String mensaje;
+    private String razon;
+    
+    public String getMensaje()
+    {
+        return mensaje;
+    }
+    public void setMensaje(String msj)
+    {
+        mensaje = msj;
+    }
+     public String getRazon()
+    {
+        return razon;
+    }
+    public void setRazon(String raz)
+    {
+        razon = raz;
+    }
+     public Integer getId()
+    {
+        return id;
+    }
+     public void setId(Integer id)
+     {
+         this.id = id;
+     }
+    public List<String> getPics()
+    {
+        return pics;
+    }
+    public void setPics(List<String> pics)
+    {
+        this.pics = pics;
+    }
+     
+}
---
+++ s3_escarabajos-web/src/main/java/co/edu/uniandes/csw/escarabajos/dtos/ReclamoDTO.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.escarabajos.dtos;
+
+import co.edu.uniandes.csw.escarabajos.entities.ReclamoEntity;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author n.gaitan
+ */
+public class ReclamoDTO 
+{
+    private Integer id;
+    private List<String> pics;
+    private String mensaje;
+    private String razon;
+   
+    public ReclamoDTO()
+    {
+        pics = new ArrayList<String>();
+    }
+     /**
+     * Conviertir Entity a DTO (Crea un nuevo DTO con los valores que recibe en
+     * la entidad que viene de argumento.
+     *
+     * @param bici: Es la entidad que se va a convertir a DTO
+     */
+    public ReclamoDTO(ReclamoEntity recl) {
+        this.id = recl.getId();
+        this.pics = recl.getPics();
+        this.mensaje = recl.getMensaje();
+        this.razon = recl.getRazon();
+    }
+    public String getMensaje()
+    {
+        return mensaje;
+    }
+    public void setMensaje(String msj)
+    {
+        mensaje = msj;
+    }
+     public String getRazon()
+    {
+        return razon;
+    }
+    public void setRazon(String raz)
+    {
+        razon = raz;
+    }
+     public Integer getId()
+    {
+        return id;
+    }
+    public List<String> getPics()
+    {
+        return pics;
+    }
+    public void setPics(List<String> pics)
+    {
+        this.pics = pics;
+    }
+    public ReclamoEntity toEntity()
+    {
+        ReclamoEntity entity = new ReclamoEntity();
+        entity.setMensaje(this.mensaje);
+        entity.setPics(this.pics);
+        entity.setId(this.id);
+        entity.setRazon(this.razon);
+        return entity;
+    }
+}
