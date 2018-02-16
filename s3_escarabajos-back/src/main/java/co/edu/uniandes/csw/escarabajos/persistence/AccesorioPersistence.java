4e8beee2f8b1ed514cf2a6912e7ba0521ad2e03b
af.varon <=>
2018-02-16 15:15:02 -0500

    Persistencia de Accesorios y Modelos

---
+++ s3_escarabajos-back/src/main/java/co/edu/uniandes/csw/escarabajos/persistence/AccesorioPersistence.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.escarabajos.persistence;
+
+import co.edu.uniandes.csw.escarabajos.entities.AccesorioEntity;
+import java.util.List;
+import java.util.logging.Logger;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+/**
+ *
+ * @author Andres
+ */
+public class AccesorioPersistence {
+    
+    public static final Logger LOGGER = Logger.getLogger(AccesorioPersistence.class.getName());
+    
+    @PersistenceContext(unitName = "EscarabajosPU")
+    protected EntityManager em;
+    
+    /**
+     *
+     * @param entity objeto accesorio que se creará en la base de datos
+     * @return devuelve la entidad creada con un id dado por la base de datos.
+     */
+    public AccesorioEntity create(AccesorioEntity entity){
+        em.persist(entity);
+        return entity;
+    }
+
+    public AccesorioEntity find(Long id){
+        return em.find(AccesorioEntity.class,id);
+    }
+    
+    public void delete(AccesorioEntity accesorio){
+       em.remove(accesorio);
+    }
+    public AccesorioEntity findByName(String name){
+         TypedQuery query = em.createQuery("Select e From AccesorioEntity e where e.name = :name", AccesorioEntity.class);
+        // Se remplaza el placeholder ":name" con el valor del argumento 
+        query = query.setParameter("name", name);
+        // Se invoca el query se obtiene la lista resultado
+        List<AccesorioEntity> sameName = query.getResultList();
+        if (sameName.isEmpty()) {
+            return null;
+        } else {
+            return sameName.get(0);
+        }
+    }
+    
+     public List<AccesorioEntity> findAll() {
+        LOGGER.info("Consultando todas los Accesorios");
+        TypedQuery query = em.createQuery("select u from AccesorioEntity u", AccesorioEntity.class);
+        return query.getResultList();
+    }
+     
+     public AccesorioEntity update(AccesorioEntity accesorio){
+        return  em.merge(accesorio);
+     }
+}
+
