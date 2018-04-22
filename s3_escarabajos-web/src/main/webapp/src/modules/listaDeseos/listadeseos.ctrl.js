7fa7d0fe57a356f1ddf60d56c8df4f569428981f
m.devia <m.devia@ISIS2603S3-0084.sis.virtual.uniandes.edu.co>
2018-04-21 21:50:36 -0500

    Interfaz con carrito con la combinacion de items y lista deseos empezada

---
+++ s3_escarabajos-web/src/main/webapp/src/modules/listaDeseos/listadeseos.ctrl.js
@@ -0,0 +1,44 @@
+(function (ng) {
+    var mod = ng.module("ListadeseosMod");
+    mod.controller('listadeseosCtrl', ['$scope', '$http',
+        function ($scope, $http) {
+            $http.get('data/items.listaDeseos.json').then(function (response) {
+                $scope.listaDeseos = response.data;
+                
+                var lista2 = [];
+                
+                var lista = $scope.listaDeseos.items;
+                
+                for (i=0;i<lista.length; i++) {
+                    
+                    var repetido = false;
+                    
+                    for (j=0;j<lista2.length; j++) {
+                        
+                        if(lista[i].color===lista[j].color  && lista[i].modelo===lista[j].modelo && lista[i].precio===lista[j].precio){
+                            
+                            lista2[j].cantidad = lista2[j].cantidad + 1;
+                            
+                            repetido = true;
+                        }
+                    }
+                    
+                    if (repetido === false){
+                        
+                        lista[i].cantidad = 1;
+                        
+                        lista2.push(lista[i]);
+                    }
+                }
+               
+                
+                $scope.listaDeseos.items = lista2;
+                
+
+            });
+            
+            
+        }
+    ]);
+}
+)(window.angular);
