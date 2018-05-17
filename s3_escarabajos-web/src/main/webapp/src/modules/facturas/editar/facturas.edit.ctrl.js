172b58f8b5f7f1d756f477b82d66a605661e1720
m.devia <m.devia@ISIS2603S3-0084.sis.virtual.uniandes.edu.co>
2018-05-17 04:15:00 -0500

    documentacion factura

--- s3_escarabajos-web/src/main/webapp/src/modules/facturas/editar/facturas.edit.ctrl.js
+++ s3_escarabajos-web/src/main/webapp/src/modules/facturas/editar/facturas.edit.ctrl.js
@@ -1,4 +1,22 @@
 (function (ng) {
+    /**
+     * @ngdoc controller
+     * @name factura.detail.update.controller:facturaEditCtrl
+     * @description
+     * Definición del controlador de Angular del módulo del update de un Factura. 
+     * Se crea el controlador con el cual se maneja el módulo.
+     * En el controlador se definen los atributos y métodos que pueden
+     * ser accedidos desde el HTML utilizando el $scope.
+     * @param {Object} $scope Referencia injectada al Scope definida para este
+     * controlador, el scope es el objeto que contiene las variables o 
+     * funciones que se definen en este controlador y que son utilizadas 
+     * desde el HTML.
+     * @param {Object} $http Objeto injectado para la manejar consultas HTTP
+     * @param {Object} accesorioContext Constante injectada que contiene la ruta
+     * donde se encuentra el API de Facturas en el Backend.
+     * @param {Object} $state Dependencia injectada en la que se recibe el 
+     * estado actual de la navegación definida en el módulo.
+     */
     var mod = ng.module("moduloFacturas");
 
     mod.constant("facturaContext", "api/facturas");
@@ -8,6 +26,21 @@
 
 
             if ($state.params.facturaId !== undefined && $state.params.facturaId !== null) {
+
+            /**
+                 * @ngdoc function
+                 * @name getFactura
+                 * @methodOf accesorios.detail.controller:FacturaEditCtrl
+                 * @description
+                 * Esta función utiliza el protocolo HTTP para obtener el recurso 
+                 * donde se encuentra el accesorio en formato JSON. El recurso
+                 * puede ser un archivo o un API Rest. La función se ejecuta
+                 * automáticamente cuando el controlador es accedido desde el
+                 * navegador.
+                 * @param {String} URL Dirección donde se encuentra el recurso
+                 * de los facturas o API donde se puede consultar. Se utiliza el
+                 * contexto definido anteriormente.
+                 */
 
                 $http.get(facturaContext + '/' + $state.params.facturaId).then(function (response) {
 
--- s3_escarabajos-web/src/main/webapp/src/modules/facturas/facturas.ctrl.js
+++ s3_escarabajos-web/src/main/webapp/src/modules/facturas/facturas.ctrl.js
@@ -1,4 +1,23 @@
 (function(ng){
+    
+    /**
+         * @ngdoc controller
+         * @name factura.controller:accesorioCtrl
+         * @description
+         * Definición del controlador de Angular del módulo Factura. 
+         * Se crea el controlador con el cual se maneja el módulo.
+         * En el controlador se definen los atributos y métodos que pueden
+         * ser accedidos desde el HTML utilizando el $scope.
+         * @param {Object} $scope Referencia injectada al Scope definida para este
+         * controlador, el scope es el objeto que contiene las variables o 
+         * funciones que se definen en este controlador y que son utilizadas 
+         * desde el HTML.
+         * @param {Object} $http Objeto injectado para la manejar consultas HTTP
+         * @param {Object} accesorioContext Constante injectada que contiene la ruta
+         * donde se encuentra el API de Facturas en el Backend.
+         * @param {Object} $state Dependencia injectada en la que se recibe el 
+         * estado actual de la navegación definida en el módulo.
+         */
     var mod = ng.module("moduloFacturas");
     
     mod.constant("facturaContext","api/facturas");
@@ -7,7 +26,20 @@
         function($scope,$http,facturaContext){
             
             $scope.facturas ={};
-            
+            /**
+             * @ngdoc function
+             * @name getFacturas
+             * @methodOf facturas.controller:accesorioCtrl
+             * @description
+             * Esta función utiliza el protocolo HTTP para obtener el recurso 
+             * donde se encuentran los accesorios en formato JSON. El recurso
+             * puede ser un archivo o un API Rest. La función se ejecuta
+             * automáticamente cuando el controlador es accedido desde el
+             * navegador.
+             * @param {String} URL Dirección donde se encuentra el recurso
+             * de los accesorios o API donde se puede consultar. Se utiliza el
+             * contexto definido anteriormente.
+             */
             $http.get(facturaContext).then(function(response){
                 $scope.facturas = response.data;
             });
