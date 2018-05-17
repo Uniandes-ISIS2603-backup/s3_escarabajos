51e72ccd87d16a65a2c5e3d0b47aefd927f24cc9
s.beltran <s.beltran@ISIS2603S3-0085.sis.virtual.uniandes.edu.co>
2018-05-17 02:44:26 -0500

    anotaciones

--- s3_escarabajos-web/src/main/webapp/src/modules/carrito/carrito.mod.js
+++ s3_escarabajos-web/src/main/webapp/src/modules/carrito/carrito.mod.js
@@ -1,4 +1,18 @@
 (function (ng) {
+    /**
+     * @ngdoc overview
+     * @name carrito.module:CarritoMod
+     * @description
+     * Definición del módulo de Angular de Bicicletas. El módulo encapsula todos los 
+     * controladores y los templates HTML que estén relacionados con los Bicicletas 
+     * directamente. En la configuración del módulo se injecta la dependencia de 
+     * ui.router que es la que se utiliza para la configuración de las URLs bajo las
+     * cuales se accede al módulo. Por ejemplo, para mostrar los autores en la 
+     * URL: 'localhost:8080/cliente/id/carrito' es necesario configurar el router por 
+     * medio del stateProvider que informa a AngularJS de la relación entre la URL, 
+     * un estado definido (estado de mostrar autores), el controlador y la vista 
+     * correspondiente.
+     */
     var mod = ng.module('CarritoMod', ['ui.router']);
     var urlCarrito = '/cliente/' + sessionStorage.getItem("id") + '/carrito';
     mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
