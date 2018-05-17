79393a8a73fc6fd311b5d894e121af11a5b09b61
n.gaitan <n.gaitan@ISIS2603S3-0061.sis.virtual.uniandes.edu.co>
2018-05-16 22:36:30 -0500

    Documentando ando

--- s3_escarabajos-web/src/main/webapp/src/modules/accesorios/calificaciones.ctrl.js
+++ s3_escarabajos-web/src/main/webapp/src/modules/accesorios/calificaciones.ctrl.js
@@ -1,4 +1,22 @@
 (function (ng) {
+    /**
+     * @ngdoc controller
+     * @name calificaciones.controller:calificacionesListCtrl2
+     * @description
+     * Definición del controlador de Angular del módulo Calificaciòn. 
+     * Se crea el controlador con el cual se maneja el módulo.
+     * En el controlador se definen los atributos y métodos que pueden
+     * ser accedidos desde el HTML utilizando el $scope.
+     * @param {Object} $scope Referencia injectada al Scope definida para este
+     * controlador, el scope es el objeto que contiene las variables o 
+     * funciones que se definen en este controlador y que son utilizadas 
+     * desde el HTML.
+     * @param {Object} $http Objeto injectado para la manejar consultas HTTP
+     * @param {Object} accesorioContext Constante injectada que contiene la ruta
+     * donde se encuentra el API de Accesorios en el Backend.
+     * @param {Object} $state Dependencia injectada en la que se recibe el 
+     * estado actual de la navegación definida en el módulo.
+     */
     var mod = ng.module("AccesoriosMod");
 
     mod.constant("calificacionContext", "api/modelos");
@@ -7,14 +25,41 @@
         function ($scope, calificacionContext, $http, $state) {
             $scope.data = {};
             if ($state.params.accesorioId !== undefined && $state.params.accesorioId !== null) {
-                
-                
+
+                /**
+                 * @ngdoc function
+                 * @name getCalificaciones
+                 * @methodOf calificaciones.list.controller:calificacionesListCtrl2
+                 * @description
+                 * Esta función utiliza el protocolo HTTP para obtener el recurso 
+                 * donde se encuentran todas las calificaciones del modelo especificado en formato JSON. El recurso
+                 * puede ser un archivo o un API Rest. La función se ejecuta
+                 * automáticamente cuando el controlador es accedido desde el
+                 * navegador.
+                 * @param {String} URL Dirección donde se encuentra el recurso
+                 * del accesorio especificado o API donde se puede consultar. Se utiliza el
+                 * contexto definido anteriormente.
+                 */
                 $http.get(calificacionContext + '/' + $state.params.accesorioId + '/calificaciones').then(function (response) {
                     $scope.calificaciones = response.data;
                 });
                 $scope.createCalificacion = function ()
                 {
-                    $http.post(calificacionContext + '/' + $state.params.accesorioId + '/calificaciones',  $scope.data).then(
+                    /**
+                     * @ngdoc function
+                     * @name postCalificacion
+                     * @methodOf calificaciones.post.controller:calificacionesListCtrl2
+                     * @description
+                     * Esta función utiliza el protocolo HTTP para realizar un POST del recurso 
+                     * creado a partir de la informaciòn ingresada. El recurso
+                     * puede ser un archivo o un API Rest. La función se ejecuta
+                     * automáticamente cuando el controlador es accedido desde el
+                     * navegador.
+                     * @param {String} URL Dirección donde se encuentra el recurso
+                     * del accesorio especificado o API donde se puede consultar. Se utiliza el
+                     * contexto definido anteriormente.
+                     */
+                    $http.post(calificacionContext + '/' + $state.params.accesorioId + '/calificaciones', $scope.data).then(
                             $state.go('^.accesorioDetail', {accesorioId: $state.params.accesorioId}, {reload: true}));
                 };
             }
--- s3_escarabajos-web/src/main/webapp/src/modules/accesorios/crear/accesorios.create.ctrl.js
+++ s3_escarabajos-web/src/main/webapp/src/modules/accesorios/crear/accesorios.create.ctrl.js
@@ -1,14 +1,32 @@
 (function (ng) {
+    /**
+     * @ngdoc controller
+     * @name accesorio.create.controller:accesorioCreateCtrl
+     * @description
+     * Definición del controlador de Angular del módulo del create de un Accesorio. 
+     * Se crea el controlador con el cual se maneja el módulo.
+     * En el controlador se definen los atributos y métodos que pueden
+     * ser accedidos desde el HTML utilizando el $scope.
+     * @param {Object} $scope Referencia injectada al Scope definida para este
+     * controlador, el scope es el objeto que contiene las variables o 
+     * funciones que se definen en este controlador y que son utilizadas 
+     * desde el HTML.
+     * @param {Object} $http Objeto injectado para la manejar consultas HTTP
+     * @param {Object} accesorioContext Constante injectada que contiene la ruta
+     * donde se encuentra el API de Accesorios en el Backend.
+     * @param {Object} $state Dependencia injectada en la que se recibe el 
+     * estado actual de la navegación definida en el módulo.
+     */
     var mod = ng.module("AccesoriosMod");
-    
-    mod.constant("accesorioContext","api/accesorios");
-    
-    mod.controller('accesorioCreateCtrl',['$scope','$http','accesorioContext','$state',
-        function($scope,$http,accesorioContext,$state){
+
+    mod.constant("accesorioContext", "api/accesorios");
+
+    mod.controller('accesorioCreateCtrl', ['$scope', '$http', 'accesorioContext', '$state',
+        function ($scope, $http, accesorioContext, $state) {
 
             $scope.data = {};
 
-            
+
 
             /**
              * @ngdoc function
@@ -19,25 +37,38 @@
              * @param {Object} accesorio Objeto con la nueva de la accesorio.
              */
             $scope.createAccesorio = function () {
-                                
+
                 var input = {};
-                
+
                 input.id = 0;
-                
+
                 input.precio = parseInt($scope.data.precio);
-                
+
                 input.modeloId = parseInt($scope.data.modeloId);
-                
+
                 input.color = $scope.color;
-                
+
                 input.album = [];
-                
+
                 input.tipoAccesorio = $scope.tipoAccesorio;
-                
+
                 input.descripcion = $scope.descripcion;
-                
+                /**
+                 * @ngdoc function
+                 * @name getAccesorio
+                 * @methodOf accesorios.create.controller:accesorioEditCtrl
+                 * @description
+                 * Esta función utiliza el protocolo HTTP para eliminar el recurso 
+                 * donde se encuentra el accesorio en formato JSON. El recurso
+                 * puede ser un archivo o un API Rest. La función se ejecuta
+                 * automáticamente cuando el controlador es accedido desde el
+                 * navegador.
+                 * @param {String} URL Dirección donde se encuentra el recurso
+                 * de los accesorios o API donde se puede consultar. Se utiliza el
+                 * contexto definido anteriormente.
+                 */
                 $http.post(accesorioContext, input).then(function (response) {
-                    
+
                     $state.go('accesoriosList', {accesorioId: response.data.id}, {reload: true});
 
                 });
